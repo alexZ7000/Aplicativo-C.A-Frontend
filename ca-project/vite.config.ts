700de71b64ee6062cad910923eb8afd32e236e74
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-12-17 18:41:36 -0300

    Teste de husky

--- ca-project/vite.config.ts
+++ ca-project/vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react({
-            tsDecorators: true,
-            devTarget: "es2022",
-            plugins: [["@swc/plugin-styled-components", {}]],
-        }),
-    ],
+  plugins: [
+    react({
+      tsDecorators: true,
+      devTarget: "es2022",
+      plugins: [["@swc/plugin-styled-components", {}]],
+    }),
+  ],
 });
